d4a826194c4ec15ce2261f6a6380e09f87587911
Sunil <sunil.ramesh@smartbear.com>
2025-01-14 18:31:19 +0530

    Handled negative numbers and sum

--- code.js
+++ code.js
@@ -4,12 +4,21 @@ class StringCalc {
         if (!numbers) {
             return 0;
         }
-
+        //delimiter handling
         let delimiter = /,|\n/;
         if (numbers.startsWith('//')) {
             const delimiterMatch = numbers.match(/^\/\/(.+)\n/)
             delimiter = new RegExp(delimiterMatch[1]);
             numbers = numbers.slice(delimiterMatch[0].length);
         }
+        const numberArray = numbers.split(delimiter).map(num => parseInt(num, 10));
+
+        //check for negative numbers
+        const negatives = numberArray.filter(num => num < 0);
+        if (negatives.length > 0) {
+            throw new Error('negative numbers are not allowed');
+        }
+        return numberArray.reduce((sum,num) => sum + (isNan(num) ? 0 : num), 0);
     }
 }
+MediaSourceHandle.exports = StringCalc;
